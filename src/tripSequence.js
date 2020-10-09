784c5ed771e0cc2be9283bc94bfa88c764c0bff1
Grant McKenna <grant.mckenna@madetech.com>
2020-10-09 15:57:53 +0100

    Amend test to run multiple bags and one goose is invalid

--- src/tripSequence.js
+++ src/tripSequence.js
@@ -5,30 +5,41 @@ class TripSequence {
         let leftBank = [];
         let rightBank = [];
 
-        leftBank.push("Farmer");
+        leftBank[0] = "Farmer";
+
+        let oddCounter = 1;
         for (var c = 1; c <= numberOfBags; c++) {
-            leftBank.push("Bag");
+            leftBank[oddCounter] = "Bags"
+            oddCounter += 2;
         }
+        let evenCounter = 2;
         for (var g = 1; g <= numberOfGeese; g++) {
-            leftBank.push("Goose");
+            leftBank[evenCounter] = "Goose";
+            evenCounter += 2;
         }
 
         for (var i = 1; i <= leftBank.length; i++) {
-            rightBank.push = leftBank.pop();
+            rightBank.push = leftBank[1];
+
+            leftBank.shift();
 
             var farmerIsOnLeftBank = 1 % 2 === 0;
 
             if (farmerIsOnLeftBank) {
-                var bagsOnRightBank = rightBank.filter(function (item) { return item === "Bag" }).length;
+                var bagsOnRightBank = rightBank.filter(function (item) { return item === "Bags" }).length;
                 var geeseOnRightBank = rightBank.filter(function (item) { return item === "Goose" }).length;
+
+                console.log("Right bank counts: Bags - " + bagsOnRightBank + " | Geese - " + geeseOnRightBank);
 
                 if (bagsOnRightBank > 0 && geeseOnRightBank > 0) {
                     result = false;
                     break;
                 }
             } else {
-                var bagsOnLeftBank = leftBank.filter(function (item) { return item === "Bag" }).length;
+                var bagsOnLeftBank = leftBank.filter(function (item) { return item === "Bags" }).length;
                 var geeseOnLeftBank = leftBank.filter(function (item) { return item === "Goose" }).length;
+
+                console.log("Left bank counts: Bags - " + bagsOnLeftBank + " | Geese - " + geeseOnLeftBank);
 
                 if (bagsOnLeftBank > 0 && geeseOnLeftBank > 0) {
                     result = false;
--- src/tripSequence.test.js
+++ src/tripSequence.test.js
@@ -9,6 +9,15 @@ it("test combination one bag of corn and zero geese is valid", () => {
     expect(isValid).toBe(true);
 })
 
+it("test combination one bag of corn and zero geese is valid", () => {
+    const numberOfBags = 1;
+    const numberOfGeese = 0;
+
+    let isValid = new TripSequence().IsValid(numberOfBags, numberOfGeese);
+
+    expect(isValid).toBe(true);
+})
+
 it("test combination nine bag of corn and zero geese is valid", () => {
     const numberOfBags = 9;
     const numberOfGeese = 0;
@@ -36,3 +45,11 @@ it("test combination zero bags of corn and nine geese is valid", () => {
     expect(isValid).toBe(true);
 })
 
+it("test combination three bags of corn and one goose is invalid", () => {
+    const numberOfBags = 3;
+    const numberOfGeese = 1;
+
+    let isValid = new TripSequence().IsValid(numberOfBags, numberOfGeese);
+
+    expect(isValid).toBe(false);
+})
