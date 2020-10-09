377f523eda45047e5a4511aadefccba5d3b32724
Grant McKenna <grant.mckenna@madetech.com>
2020-10-09 08:47:17 +0100

    Add logo and set initial background and font color

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <div className="row">
+          <div className="col-md-12 col-sm-12 col-xss-12">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Welcome Farmer John!</h1>
+          </div>
+        </div>
+
+      </section>
     </div>
   );
 }
