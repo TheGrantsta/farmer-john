2e9247b0d36b5f4774b794872f0b11de495b4ff0
Grant McKenna <grant.mckenna@madetech.com>
2020-10-09 14:46:39 +0100

    Amend trip sequence to include tests for multiple geese and no bags of corn

--- src/tripSequence.test.js
+++ src/tripSequence.test.js
@@ -17,3 +17,21 @@ it("test combination nine bag of corn and zero geese is valid", () => {
 
     expect(isValid).toBe(true);
 })
+
+it("test combination zero bags of corn and one goose is valid", () => {
+    const numberOfBags = 0;
+    const numberOfGeese = 1;
+
+    let isValid = new TripSequence().IsValid(numberOfBags, numberOfGeese);
+
+    expect(isValid).toBe(true);
+})
+
+it("test combination zero bags of corn and nine geese is valid", () => {
+    const numberOfBags = 0;
+    const numberOfGeese = 9;
+
+    let isValid = new TripSequence().IsValid(numberOfBags, numberOfGeese);
+
+    expect(isValid).toBe(true);
+})
