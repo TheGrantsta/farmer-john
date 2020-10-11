0ff6bf8d5543204e7f8769817aecde025c75ad1d
Grant McKenna <grant.mckenna@madetech.com>
2020-10-11 08:08:11 +0100

    Amend trip steps to support just bags of corn

--- src/tripSteps.js
+++ src/tripSteps.js
@@ -3,13 +3,21 @@ import React from "react"
 function TripSteps(numberOfBags, numberOfGeese) {
     var steps = {
         IsValid: false,
-        Sequence: "Your corn will be eaten!",
+        Sequence: "",
         Repeat: 0
     };
 
     if (numberOfBags >= 2 && numberOfGeese > 1) {
-        return steps;
+        steps.Sequence = "Your corn will be eaten!";
     }
+
+    if (numberOfBags > 0 && numberOfGeese === 0) {
+        steps.IsValid = true;
+        steps.Sequence = "take a bag of corn; come back";
+        steps.Repeat = numberOfBags;
+    }
+
+    return steps;
 }
 
 export default TripSteps
--- src/tripSteps.test.js
+++ src/tripSteps.test.js
@@ -1,6 +1,6 @@
 import TripSteps from "./tripSteps"
 
-it("test combination two bags of corn and two goose is valid", ()=>{
+it("test combination two bags of corn and two goose is invalid", ()=>{
     const numberOfBags = 2;
     const numberOfGeese = 2;
 
@@ -9,4 +9,15 @@ it("test combination two bags of corn and two goose is valid", ()=>{
     expect(steps.IsValid).toBe(false);
     expect(steps.Sequence).toBe("Your corn will be eaten!");
     expect(steps.Repeat).toBe(0);
+})
+
+it("test combination two bags of corn and no geese is valid", ()=>{
+    const numberOfBags = 2;
+    const numberOfGeese = 0;
+
+    let steps = new TripSteps(numberOfBags, numberOfGeese);
+
+    expect(steps.IsValid).toBe(true);
+    expect(steps.Sequence).toBe("take a bag of corn; come back");
+    expect(steps.Repeat).toBe(2);
 })
